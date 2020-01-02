af754d15bc26b8ac928ff517699aa1be11af1e0b
jimmy483 <jimmy_sherpa483@hotmail.com>
2020-01-02 18:15:22 +0545

    login updated

--- app/src/main/java/com/example/taskmanager/MainActivity.java
+++ app/src/main/java/com/example/taskmanager/MainActivity.java
@@ -5,15 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.taskmanager.api.UserAPI;
+import com.example.taskmanager.model.DetailCheck;
+import com.example.taskmanager.serverresponse.SignUpResponse;
+import com.example.taskmanager.url.URL;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 TextView create;
+EditText user,pass;
+Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         create=findViewById(R.id.txtCreate);
+        user=findViewById(R.id.etUser);
+        pass=findViewById(R.id.etPass);
+        button=findViewById(R.id.btnLog);
         create.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,5 +38,35 @@ TextView create;
                 startActivity(intent);
             }
         });
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    public void login()
+    {
+        String name=user.getText().toString();
+        String password=pass.getText().toString();
+
+        DetailCheck detailCheck=new DetailCheck(name,password);
+        UserAPI userAPI= URL.getInstance().create(UserAPI.class);
+        Call<SignUpResponse> loginCall=userAPI.checkUser(name,password);
+        loginCall.enqueue(new Callback<SignUpResponse>() {
+            @Override
+            public void onResponse(Call<SignUpResponse> call, Response<SignUpResponse> response) {
+                Toast.makeText(MainActivity.this,"Login Successful",Toast.LENGTH_LONG);
+            }
+
+            @Override
+            public void onFailure(Call<SignUpResponse> call, Throwable t) {
+
+                Toast.makeText(MainActivity.this,t.getMessage(),Toast.LENGTH_LONG);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/taskmanager/model/DetailCheck.java
@@ -0,0 +1,29 @@
+package com.example.taskmanager.model;
+
+import android.os.StrictMode;
+
+public class DetailCheck {
+    private String username;
+    private String password;
+
+    public DetailCheck(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
